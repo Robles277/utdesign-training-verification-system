c6b6aa8984a75ffbe247955058c5bb94463f3127
unknown <ssoname78@gmail.com>
2021-03-18 23:47:22 -0500

    Adding launch and test page

--- training-verification-ui/src/app/app-routing.module.ts
+++ training-verification-ui/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LaunchComponent } from './views/launch/launch.component';
+import { TestComponent } from './views/test/test.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'launch', component: LaunchComponent },
+  { path: 'test', component: TestComponent },
+  { path: '', component: LaunchComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ training-verification-ui/src/app/views/launch/launch.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-launch',
+  templateUrl: './launch.component.html',
+  styleUrls: ['./launch.component.css']
+})
+export class LaunchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  
+  
+  }
+
+}
